c9b213615f9ea349e65094c084aa2d79ceab5cc3
ℳUHAMME⫸ <60465352+msn560@users.noreply.github.com>
2023-04-13 22:31:17 +0300

    Add files via upload

---
+++ chromeDirver/execute_script/navigator.js
@@ -0,0 +1,12 @@
+
+Object.defineProperty(navigator, 'languages', {
+    get: function () {
+        return ['en-US', 'en'];
+    },
+});
+
+Object.defineProperty(navigator, 'plugins', {
+    get: function () {
+        return [1, 2, 3, 4, 5];
+    },
+});
